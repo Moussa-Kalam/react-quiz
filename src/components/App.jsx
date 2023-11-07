4ff94aa9e46a2692422e2f43d40eb6b8b3cf54e1
Moussa-Kalam <m.amzat@alustudent.com>
2023-11-07 23:40:02 +0200

    feat: handle new answers and update points

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,9 +13,13 @@ const initialState = {
   // "loading", "error", "ready", "active", "finished"
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
+  const currentQuestion = state.questions.at(state.index);
+
   switch (action.type) {
     case "dataReceived":
       return {
@@ -35,13 +39,23 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === currentQuestion.correctOption
+            ? state.points + currentQuestion.points
+            : state.points,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -65,7 +79,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen questionsNum={questionsNum} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </MainSection>
     </div>
   );
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable react/prop-types */
+import classNames from "classnames";
 
-export const Options = ({ question }) => {
+export const Options = ({ question, dispatch, answer }) => {
+  const hasAnswered = answer !== null;
+
   return (
     <div className='options'>
-      {question.options.map((option) => (
-        <button className='btn btn-option' key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={classNames("btn btn-option", {
+            answer: index === answer,
+
+            correct: hasAnswered && index === question.correctOption,
+
+            wrong: hasAnswered && index !== question.correctOption,
+          })}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
